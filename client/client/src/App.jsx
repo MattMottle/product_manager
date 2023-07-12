c21231aabdadaeafb3a08b18b78dc6348d5d8d2b
Matt Mottle <mattmottle1@gmail.com>
2023-07-11 19:47:11 -0700

    second commit

--- client/client/src/App.jsx
+++ client/client/src/App.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
-import ProductFrom from "./components/ProductForm";
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Main from './views/Main';
+import Detail from './components/Detail';
 
-function App () {
-  return (
-    <div className="App">
-      <ProductFrom/>
-    </div>
-  );
+const App = () => {
+  
+    return(
+      <div>
+        <BrowserRouter>
+          <Routes>
+              <Route element={<Main/>} path="/products" default/>
+              <Route element={<Detail/>} path="/products/:id"/>
+          </Routes>
+        </BrowserRouter>
+      </div>
+    )
 }
 export default App;
--- server/controllers/product.controller.js
+++ server/controllers/product.controller.js
@@ -1,5 +1,29 @@
 const Product = require('../models/product.model');
 
+module.exports.getAllProducts = (req, res) => {
+    Product.find({})
+        .then(allProducts => {
+            // console.log(allProducts);
+            res.json(allProducts);
+        })
+        .catch(err => {
+            console.log(err);
+            res.json(err);
+        })
+}
+
+module.exports.getProduct = (req, res) => {
+    Product.findOne({_id: req.params.id})
+        .then(product => {
+            console.log(product); 
+            res.json(product);
+        })
+        .catch(err => { 
+            console.log(err);
+            res.json(err);
+        })
+}
+
 module.exports.createProduct = (req, res) => {
     Product.create(req.body)
         .then(newProduct => res.json(newProduct))
